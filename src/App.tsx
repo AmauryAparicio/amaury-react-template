649e712f0bbdfe9060f7585472021549ab6db078
Amaury Aparicio <amaurysunstar@gmail.com>
2020-12-17 21:10:58 -0600

    Redux starter kit applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./router/Routes";
 import SubRouter from "./router/SubRouter";
+import store from "./store/store";
 import "./styles/styles.scss";
 
 const App = () => {
   return (
-    <Router>
-      <SubRouter routes={Routes} />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <SubRouter routes={Routes} />
+      </Router>
+    </Provider>
   );
 };
 
